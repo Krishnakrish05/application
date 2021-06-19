88fc62ec6a45e898a66bb2ec25674f16d54ff998
Krishnakrish05 <47746860+Krishnakrish05@users.noreply.github.com>
2021-06-19 21:23:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 def app():
      try:
-     name= input("Enter the name:")
-     print(name)
-     age = int(input("Your age:"))
-     print(age)
-     gender= input("Enter your gender:")
-     if (gender!='male') or  (gender!='female'):
-          raise Exeption
-     print(gender)
-     salary=int(input("Your salary:"))
-     print(salary)
-     state= input("Your State:")
-     if(len(state)>=5):
+       name= input("Enter the name:")
+       print(name)
+       age = int(input("Your age:"))
+       print(age)
+       gender= input("Enter your gender:")
+       if (gender!='male') or  (gender!='female'):
+           raise Exeption
+      print(gender)
+      salary=int(input("Your salary:"))
+      print(salary)
+      state= input("Your State:")
+      if(len(state)>=5):
           print(state)
-     else:
+      else:
           print("Enter the correct state")
-     city= input("Your city:")
-     if(len(city)>=4):
+      city= input("Your city:")
+      if(len(city)>=4):
           print(city)
-     else:
+      else:
           print("Enter the correct city")
          
      
-     except:
+    except:
           print('Enter the correct gender')
+          #Similarly all exceptions
           
 
 app()
