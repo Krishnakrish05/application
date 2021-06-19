4a32024b09dd4bfb1a30550bedf2097ec04450a8
Krishnakrish05 <47746860+Krishnakrish05@users.noreply.github.com>
2021-06-19 20:50:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 def app():
+     try:
      name= input("Enter the name:")
      print(name)
      age = int(input("Your age:"))
      print(age)
      gender= input("Enter your gender:")
-     if (gender=='male') or  (gender=='female'):
-          print(gender)
-     else:
-          print("Enter the correct gender")
+     if (gender!='male') or  (gender!='female'):
+          raise Exeption
+     print(gender)
      salary=int(input("Your salary:"))
      print(salary)
      state= input("Your State:")
@@ -20,5 +20,10 @@ def app():
           print(city)
      else:
           print("Enter the correct city")
+         
+     
+     except:
+          print('Enter the correct gender')
+          
 
 app()
